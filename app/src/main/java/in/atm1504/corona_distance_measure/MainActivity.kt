095eb823268ade82a4b7330cd5b5d071e977b069
Amartya Mondal <hayyoulistentome@gmail.com>
2020-03-30 12:04:30 +0530

    Initial commit

---
+++ app/src/main/java/in/atm1504/corona_distance_measure/MainActivity.kt
@@ -0,0 +1,19 @@
+package `in`.atm1504.corona_distance_measure
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import `in`.atm1504.corona_distance_measure.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/in/atm1504/corona_distance_measure/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package `in`.atm1504.corona_distance_measure.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
